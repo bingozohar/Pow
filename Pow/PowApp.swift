80327354bfad93e23f21141c8c25c8d814aca20f
Romi <romi.peli@pm.me>
2023-03-26 01:32:51 +0100

    Initial Commit

---
+++ Pow/PowApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowApp.swift
+//  Pow
+//
+//  Created by Romi on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
